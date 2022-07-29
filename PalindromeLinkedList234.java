c96842714d20677c2b7be0ef78d405e11a29a7a0
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2022-07-29 09:20:11 +0000

    Updated PalindromeLinkedList234.java

--- PalindromeLinkedList234.java
+++ PalindromeLinkedList234.java
@@ -48,22 +48,23 @@ class PalindromeLinkedList234 {
       
     return true;
   }
-}
 
-class ListNode {
-  int val;
-  ListNode next;
-  ListNode() {}
-  ListNode(int val) { this.val = val; }
-  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     
-  public String toString() {
-    String result = val + " ";
+    public String toString() {
+      String result = val + " ";
       
-    if (next != null) {
-      result += next.toString();
+      if (next != null) {
+        result += next.toString();
+      }
+      
+      return result;
     }
-      
-    return result;
   }
 }
+
