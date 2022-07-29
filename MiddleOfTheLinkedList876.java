8305c829afec08ce62a2ccd9bf5ab30c0887bc1a
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2022-07-29 08:53:08 +0000

    Added MiddleOfTheLinkedList876.java

---
+++ MiddleOfTheLinkedList876.java
@@ -0,0 +1,42 @@
+class MiddleOfTheLinkedList876 {
+  private ListNode current;
+  private int size;
+  private int midpoint;
+    
+  public ListNode middleNode(ListNode head) {
+    current = head;
+
+    while (current != null) {
+      current = current.next;
+      size++;
+    }
+
+    midpoint = size / 2 + 1; 
+    current = head;
+
+    while (midpoint > 1) {
+      current = current.next;
+      midpoint--;
+    }
+
+    return current;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode() {}
+  ListNode(int val) { this.val = val; }
+  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+  public String toString() {
+    String result = val + " ";
+      
+    if (next != null) {
+      result += next.toString();
+    }
+      
+    return result;
+  }
+}
