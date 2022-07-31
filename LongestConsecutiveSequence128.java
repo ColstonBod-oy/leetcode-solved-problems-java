2ff8b727ab2490e8faaa0a2c5e08f166f2734055
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2022-07-31 06:50:27 +0000

    Added LongestConsecutiveSequence128.java

---
+++ LongestConsecutiveSequence128.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+class LongestConsecutiveSequence128 {
+  public int longestConsecutive(int[] nums) {
+    HashSet<Integer> set = new HashSet<>(Arrays.stream(nums).boxed().collect(Collectors.toSet()));
+    int res = 0;
+
+    for (int num : nums) {
+      int counter = 0;
+      int nextNum = 0;
+      
+      if (!set.contains(num - 1)) {
+        ++counter;
+        nextNum = num + 1;
+
+        while (set.contains(nextNum)) {
+          ++counter;
+          ++nextNum;
+        }
+
+        if (counter > res) {
+          res = counter;
+        }
+      }
+    }
+    
+    return res;
+  }
+}
