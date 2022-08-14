d0669584b65778af2919296a932bb84d87676182
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2022-08-14 07:34:58 +0000

    Updated PermutationInString567.java

--- PermutationInString567.java
+++ PermutationInString567.java
@@ -8,7 +8,7 @@ class PermutationInString567 {
 
     int leftPtr = 0;
     int rightPtr = s1.length() - 1;
-    int matches = 26;
+    int matches = 0;
     
     HashMap<Character, Integer> mapS1 = new HashMap<>() {
       {
@@ -22,59 +22,42 @@ class PermutationInString567 {
 
     for (int i = 0; i <= rightPtr; i++) {
       mapS1.compute(Character.valueOf(s1.charAt(i)), (key, val) -> ++val);
+      mapS2.compute(Character.valueOf(s2.charAt(i)), (key, val) -> ++val);
+    }
 
-      if (mapS1.get(Character.valueOf(s1.charAt(i))) == 1) {
-        --matches;
+    for (char c = 'a'; c <= 'z'; c++) {
+      if (mapS1.get(Character.valueOf(c)) == mapS2.get(Character.valueOf(c))) {
+        ++matches;
       }
     }
 
-    for (int i = 0; i <= rightPtr; i++) {
-      System.out.println(mapS1);
-      System.out.println(mapS2);
-      System.out.println(matches);
-      System.out.println("---------------------");
-      mapS2.compute(Character.valueOf(s2.charAt(i)), (key, val) -> ++val);
-
-      if (mapS1.get(Character.valueOf(s2.charAt(i))) == mapS2.get(Character.valueOf(s2.charAt(i)))) {
-        ++matches;
-      }
-
-      else if (mapS1.get(Character.valueOf(s2.charAt(i))) <= 1 && mapS2.get(Character.valueOf(s2.charAt(i))) <= 2) {
-        --matches;
-      }
-
-      if (matches == 26) {
-        return true;
-      }
+    if (matches == 26) {
+      return true;
     }
     
     while (rightPtr < s2.length() - 1) {
-      System.out.println(mapS2);
-      System.out.println(matches);
-      System.out.println("---------------------");
-
       mapS2.compute(Character.valueOf(s2.charAt(leftPtr)), (key, val) -> --val);
 
-      if (mapS1.get(Character.valueOf(s2.charAt(leftPtr))) == mapS2.get(Character.valueOf(s2.charAt(leftPtr)))) {
-        ++matches;
+      if (mapS2.get(Character.valueOf(s2.charAt(leftPtr))) + 1 == mapS1.get(Character.valueOf(s2.charAt(leftPtr)))) {
+        --matches;
       }
 
-      else if (mapS2.get(Character.valueOf(s2.charAt(leftPtr))) <= 1) {
-        --matches;
+      else if (mapS2.get(Character.valueOf(s2.charAt(leftPtr))) == mapS1.get(Character.valueOf(s2.charAt(leftPtr)))) {
+        ++matches;
       }
       
       ++leftPtr;
       ++rightPtr;
       mapS2.compute(Character.valueOf(s2.charAt(rightPtr)), (key, val) -> ++val);
 
-      if (mapS1.get(Character.valueOf(s2.charAt(rightPtr))) == mapS2.get(Character.valueOf(s2.charAt(rightPtr)))) {
-        ++matches;
-      }
-
-      else if (mapS1.get(Character.valueOf(s2.charAt(rightPtr))) <= 1 && mapS2.get(Character.valueOf(s2.charAt(rightPtr))) <= 2) {
+      if (mapS2.get(Character.valueOf(s2.charAt(rightPtr))) - 1 == mapS1.get(Character.valueOf(s2.charAt(rightPtr)))) {
         --matches;
       }
 
+      else if (mapS2.get(Character.valueOf(s2.charAt(rightPtr))) == mapS1.get(Character.valueOf(s2.charAt(rightPtr)))) {
+        ++matches;
+      }
+      
       if (matches == 26) {
         return true;
       }
