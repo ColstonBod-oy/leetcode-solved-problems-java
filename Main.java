26b591a803375f7a3a3c0f7dc8e132cb6a0512d6
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2022-08-16 05:32:19 +0000

    Updated PermutationInString567.java

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    String s1 = "rvwrk";
-    String s2 = "lznomzggwrvrkxecjaq";
-    System.out.println(new PermutationInString567().checkInclusion(s1, s2));
     System.out.println("Hello world!");
   }
 }
--- PermutationInString567.java
+++ PermutationInString567.java
@@ -13,7 +13,7 @@ class PermutationInString567 {
     HashMap<Character, Integer> mapS1 = new HashMap<>() {
       {
         for (char c = 'a'; c <= 'z'; c++) {
-          put(Character.valueOf(c), 0);
+          put(c, 0);
         }
       }
     };
@@ -21,12 +21,12 @@ class PermutationInString567 {
     HashMap<Character, Integer> mapS2 = new HashMap<>(mapS1);
 
     for (int i = 0; i <= rightPtr; i++) {
-      mapS1.compute(Character.valueOf(s1.charAt(i)), (key, val) -> ++val);
-      mapS2.compute(Character.valueOf(s2.charAt(i)), (key, val) -> ++val);
+      mapS1.compute(s1.charAt(i), (key, val) -> ++val);
+      mapS2.compute(s2.charAt(i), (key, val) -> ++val);
     }
 
     for (char c = 'a'; c <= 'z'; c++) {
-      if (mapS1.get(Character.valueOf(c)) == mapS2.get(Character.valueOf(c))) {
+      if (mapS1.get(c).equals(mapS2.get(c))) {
         ++matches;
       }
     }
@@ -36,25 +36,26 @@ class PermutationInString567 {
     }
     
     while (rightPtr < s2.length() - 1) {
-      mapS2.compute(Character.valueOf(s2.charAt(leftPtr)), (key, val) -> --val);
-
-      if (mapS2.get(Character.valueOf(s2.charAt(leftPtr))) + 1 == mapS1.get(Character.valueOf(s2.charAt(leftPtr)))) {
+      if (mapS2.get(s2.charAt(leftPtr)).equals(mapS1.get(s2.charAt(leftPtr)))) {
         --matches;
       }
 
-      else if (mapS2.get(Character.valueOf(s2.charAt(leftPtr))) == mapS1.get(Character.valueOf(s2.charAt(leftPtr)))) {
+      mapS2.compute(s2.charAt(leftPtr), (key, val) -> --val);
+
+      if (mapS2.get(s2.charAt(leftPtr)).equals(mapS1.get(s2.charAt(leftPtr)))) {
         ++matches;
       }
       
       ++leftPtr;
       ++rightPtr;
-      mapS2.compute(Character.valueOf(s2.charAt(rightPtr)), (key, val) -> ++val);
 
-      if (mapS2.get(Character.valueOf(s2.charAt(rightPtr))) - 1 == mapS1.get(Character.valueOf(s2.charAt(rightPtr)))) {
+      if (mapS2.get(s2.charAt(rightPtr)).equals(mapS1.get(s2.charAt(rightPtr)))) {
         --matches;
       }
 
-      else if (mapS2.get(Character.valueOf(s2.charAt(rightPtr))) == mapS1.get(Character.valueOf(s2.charAt(rightPtr)))) {
+      mapS2.compute(s2.charAt(rightPtr), (key, val) -> ++val);
+
+      if (mapS2.get(s2.charAt(rightPtr)).equals(mapS1.get(s2.charAt(rightPtr)))) {
         ++matches;
       }
       
