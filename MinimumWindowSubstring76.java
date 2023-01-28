c5b725c48a56323033f5c0108ad1727bdf6673e9
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-01-28 07:12:38 +0000

    Updated MinimumWindowSubstring76.java

--- MinimumWindowSubstring76.java
+++ MinimumWindowSubstring76.java
@@ -2,10 +2,8 @@ import java.util.HashMap;
 
 class MinimumWindowSubstring76 {
   public String minWindow(String s, String t) {
-    String res = "";
-    
     if (t.length() > s.length()) {
-      return res;
+      return "";
     }
 
     if (s.equals(t)) {
@@ -13,58 +11,78 @@ class MinimumWindowSubstring76 {
     }
 
     int leftPtr = 0;
-    int rightPtr = 0;
     int have = 0;
-    int len = s.length();
+    int minLen = s.length() + 1;
     HashMap<Character, Integer> mapT = new HashMap<>();
 
     for (char c : t.toCharArray()) {
-      mapT.computeIfAbsent(c, key -> 0);
-      mapT.computeIfPresent(c, (key, val) -> ++val);
+      mapT.put(c, mapT.getOrDefault(c, 0) + 1);
     }
 
     int need = mapT.size();
     HashMap<Character, Integer> mapWindow = new HashMap<>(mapT);
     mapWindow.replaceAll((key, val) -> 0);
 
-    if (mapWindow.containsKey(s.charAt(leftPtr))) {
-      mapWindow.compute(s.charAt(leftPtr), (key, val) -> ++val);
-      ++have;
-      
-      if (have == need) {
-        return Character.toString(s.charAt(leftPtr));
-      }
-    }
+    for (int rightPtr = 0; rightPtr < s.length(); rightPtr++) {
+      char c = s.charAt(rightPtr);
 
-    while (rightPtr < s.length() - 1) {
-      ++rightPtr;
+      if (mapWindow.containsKey(c)) {
+        mapWindow.compute(c, (key, val) -> ++val);
 
-      if (mapWindow.containsKey(s.charAt(rightPtr))) {
-        mapWindow.compute(s.charAt(rightPtr), (key, val) -> ++val);
-        
-        if (mapWindow.get(s.charAt(rightPtr)) == mapT.get(s.charAt(rightPtr))) {
+        if (mapWindow.get(c) == mapT.get(c)) {
           ++have;
-        }  
+        }
       }
 
       while (have == need) {
-        if (s.substring(leftPtr, rightPtr + 1).length() <= len) {
-          res = s.substring(leftPtr, rightPtr + 1);
-          len = res.length();
+        int len = rightPtr - leftPtr + 1;
+        
+        if (minLen > len) {
+          minLen = len;
         }
 
-        if (mapWindow.containsKey(s.charAt(leftPtr))) {
-          mapWindow.compute(s.charAt(leftPtr), (key, val) -> --val);
+        char removed = s.charAt(leftPtr++);
+
+        if (mapWindow.containsKey(removed)) {
+          mapWindow.compute(c, (key, val) -> --val);
           
-          if (mapWindow.get(s.charAt(leftPtr)) < mapT.get(s.charAt(leftPtr))) {
+          if (mapWindow.get(c) < mapT.get(c)) {
             --have;
-          }  
+          }
         }
-
-        ++leftPtr; 
       }
     }
 
-    return res;
+    // while (rightPtr < s.length() - 1) {
+    //   ++rightPtr;
+
+    //   if (mapWindow.containsKey(s.charAt(rightPtr))) {
+    //     mapWindow.compute(s.charAt(rightPtr), (key, val) -> ++val);
+        
+    //     if (mapWindow.get(s.charAt(rightPtr)) == mapT.get(s.charAt(rightPtr))) {
+    //       ++have;
+    //     }  
+    //   }
+
+    //   while (have == need) {  
+    //     if (s.substring(leftPtr, rightPtr + 1).length() < len) {
+    //       res = s.substring(leftPtr, rightPtr + 1);
+    //       len = res.length();
+    //     }
+
+    //     if (mapWindow.containsKey(s.charAt(leftPtr))) {
+    //       mapWindow.compute(s.charAt(leftPtr), (key, val) -> --val);
+          
+    //       if (mapWindow.get(s.charAt(leftPtr)) < mapT.get(s.charAt(leftPtr))) {
+    //         --have;
+    //       }  
+    //     }
+
+    //     ++leftPtr; 
+    //   }
+    // }
+
+    // return res;
+    return s.substring(leftPtr - 1, leftPtr - 1 + minLen);
   }
 }
