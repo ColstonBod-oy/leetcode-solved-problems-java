3f21e649abfd4a232720af3f750928f4ba1fd06d
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-01-30 11:54:16 +0000

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
     // System.out.println(new ClassName().solutionMethod());
-    int[] input = {1};
-    System.out.println(Arrays.toString(new SlidingWindowMaximum239().maxSlidingWindow(input, 1)));
+    System.out.println("Hello world!");
   }
 }
