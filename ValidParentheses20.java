78e60c74c3c85e5f77804a9bf1e8583cf0722370
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-01-30 23:51:56 +0000

    Added ValidParentheses20.java

---
+++ ValidParentheses20.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+import java.util.Stack;
+
+class ValidParentheses20 {
+  public boolean isValid(String s) {
+    if (s.length() % 2 != 0) {
+      return false;
+    }
+    
+    Stack<Character> charStack = new Stack<>();
+    Map<Character, Character> charMap = Map.of(')', '(', '}', '{', ']', '[');
+
+    for (char c : s.toCharArray()) {
+      if (charMap.containsKey(c)) {
+        if (!charStack.isEmpty() && charStack.peek().equals(charMap.get(c))) {
+          charStack.pop();  
+        }
+
+        else {
+          return false;
+        }
+      }
+
+      else {
+        charStack.push(c);
+      }
+    }
+
+    return charStack.isEmpty();
+  }
+}
