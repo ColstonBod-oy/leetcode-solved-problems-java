89e2c1870c7083b8ed2068967767fbc9e925dfdc
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-02-01 06:10:41 +0000

    Added EvaluateReversePolishNotation150.java

---
+++ EvaluateReversePolishNotation150.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class EvaluateReversePolishNotation150 {
+  public int evalRPN(String[] tokens) {
+    Stack<Integer> s = new Stack<>();
+    Integer a = null, b = null;
+
+    for (String t : tokens) {
+      switch (t) {
+        case "+":
+          s.push(s.pop() + s.pop());
+          break;
+        case "-":
+          a = s.pop(); 
+          b = s.pop();
+          s.push(b - a);
+          break;
+        case "*":
+          s.push(s.pop() * s.pop());
+          break;
+        case "/":
+          a = s.pop();
+          b = s.pop();
+          s.push(b / a);
+          break;
+        default:
+          s.push(Integer.valueOf(t));
+      }
+    }
+
+    return s.peek();
+  }
+}
