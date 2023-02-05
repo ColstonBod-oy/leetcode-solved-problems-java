b11cd79afde12b6071f63b2ae1a6486bafd72205
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-02-05 11:27:16 +0000

    Added DailyTemperatures739.java

---
+++ DailyTemperatures739.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class DailyTemperatures739 {
+  public int[] dailyTemperatures(int[] temperatures) {
+    Stack<Integer> s = new Stack<>();
+    int[] res = new int[temperatures.length];
+    
+    for (int i = 0; i < temperatures.length; i++) {
+      while (!s.empty() && temperatures[i] > temperatures[s.peek()]) {
+        int j = s.pop();
+        res[j] = i - j;
+      }
+
+      s.push(i);
+    }
+
+    return res;
+  }
+}
