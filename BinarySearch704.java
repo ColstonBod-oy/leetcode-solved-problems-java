abf9f1f91fa3ee1d45c7e9d104bf63bd58c01c87
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-20 06:59:30 +0000

    Added BinarySearch704.java

---
+++ BinarySearch704.java
@@ -0,0 +1,24 @@
+class BinarySearch704 {
+  public int search(int[] nums, int target) {
+    int l = 0;
+    int r = nums.length - 1;
+
+    while (l <= r) {
+      int m = (l + r) / 2;
+      
+      if (nums[m] > target) {
+        r = --m; 
+      }
+
+      else if (nums[m] < target) {
+        l = ++m;
+      }
+
+      else {
+        return m;
+      }
+    }
+
+    return -1;
+  }
+}
