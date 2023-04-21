98d7efbdf62a72e159f11280bef2e3e944a98d84
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-21 04:15:32 +0000

    Added FindMinimumInRotatedSortedArray153.java

---
+++ FindMinimumInRotatedSortedArray153.java
@@ -0,0 +1,27 @@
+class FindMinimumInRotatedSortedArray153 {
+  public int findMin(int[] nums) {
+    int l = 0;
+    int r = nums.length - 1;
+    int res = nums[0];
+
+    while (l <= r) {
+      if (nums[l] <= nums[r]) {
+        res = Math.min(res, nums[l]);
+        break;
+      }
+
+      int m = (l + r) / 2;
+      res = Math.min(res, nums[m]);
+
+      if (nums[m] >= nums[l]) {
+        l = ++m;
+      }
+
+      else {
+        r = m;
+      }
+    }
+
+    return res;
+  }
+}
