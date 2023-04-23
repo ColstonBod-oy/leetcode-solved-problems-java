dc278967c4a8c09aa02d206f9ffb8cb9690ca215
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-23 09:48:58 +0000

    Added LinkedListCycle141.java

---
+++ LinkedListCycle141.java
@@ -0,0 +1,26 @@
+public class LinkedListCycle141 {
+  public boolean hasCycle(ListNode head) {
+    ListNode tortoise = head;
+    ListNode hare = head;
+
+    while (hare != null && hare.next != null) {
+      tortoise = tortoise.next;
+      hare = hare.next.next;
+
+      if (tortoise == hare) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+}
