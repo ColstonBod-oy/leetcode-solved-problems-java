2dc62a303a5534a050d18a8b154b750d1e597607
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-24 08:53:14 +0000

    Added FindTheDuplicateNumber287.java

---
+++ FindTheDuplicateNumber287.java
@@ -0,0 +1,24 @@
+class FindTheDuplicateNumber287 {
+  public int findDuplicate(int[] nums) {
+    int tortoise = 0;
+    int hare = 0;
+
+    do {
+      tortoise = nums[tortoise];
+      hare = nums[nums[hare]];
+    }
+
+    while (tortoise != hare);
+
+    int tortoise2 = 0;
+
+    do {
+      tortoise = nums[tortoise];
+      tortoise2 = nums[tortoise2];
+    }
+
+    while (tortoise != tortoise2);
+
+    return tortoise2;
+  }
+}
