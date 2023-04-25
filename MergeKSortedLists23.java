1ef0206885985105c9b11aa0786352ba10e8af46
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-25 11:32:34 +0000

    Updated MergeKSortedLists23.java

--- MergeKSortedLists23.java
+++ MergeKSortedLists23.java
@@ -1,57 +1,55 @@
-import java.util.PriorityQueue;
-
 class MergeKSortedLists23 {
   public ListNode mergeKLists(ListNode[] lists) {
-    if (lists == null || lists.length == 0) {
-      return null;
+    int size = lists.length;
+    int interval = 1;
+
+    while (interval < size) {
+      for (int i = 0; i < size - interval; i += 2 * interval) {
+        lists[i] = merge(lists[i], lists[i + interval]);
+      }
+
+      interval *= 2;
     }
 
-    PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> a.compareTo(b));
+    return size > 0 ? lists[0] : null;
+  }
 
-    for (ListNode n : lists) {
-      if (n != null) {
-        queue.offer(n);  
-      } 
-    }
-
+  private ListNode merge(ListNode l1, ListNode l2) {
     ListNode dummy = new ListNode(0);
     ListNode curr = dummy;
     
-    while (!queue.isEmpty()) {
-      ListNode n = queue.poll();
-      curr.next = n;
-      curr = curr.next;
-
-      if (n.next != null) {
-        queue.offer(n.next);
+    while (l1 != null && l2 != null) {
+      if (l1.val <= l2.val) {
+        curr.next = l1;
+        l1 = l1.next;
       }
+
+      else {
+        curr.next = l2;
+        l2 = l2.next;
+      }
+
+      curr = curr.next;
+    }
+
+    if (l1 != null) {
+      curr.next = l1;
+    }
+
+    else {
+      curr.next = l2;
     }
 
     return dummy.next;
   }
 
-  class ListNode implements Comparable<ListNode> {
+  class ListNode {
     int val;
     ListNode next;
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
-    @Override
-    public int compareTo(ListNode n) {
-      if (this.val < n.val) {
-        return -1;
-      }
-                             
-      else if (this.val > n.val) {
-        return 1;
-      }
-  
-      else {
-        return 0;
-      }
-    }
-    
     @Override
     public String toString() {
       String result = val + " -> ";
