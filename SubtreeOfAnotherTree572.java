5643bd720f832bfa27a00b727e0c59880294e076
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-27 07:06:00 +0000

    Added SubtreeOfAnotherTree572.java

---
+++ SubtreeOfAnotherTree572.java
@@ -0,0 +1,45 @@
+class SubtreeOfAnotherTree572 {
+  public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    if (subRoot == null || isSameTree(root, subRoot)) return true;
+    if (root == null) return false;
+
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+  }
+
+  private boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    
+    if (p == null || q == null || p.val != q.val) {
+      return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+
+    public void printTree() {
+      printTree("", this, false);
+    }
+    
+    public void printTree(String prefix, TreeNode n, boolean isRight) {
+      if (n != null) {
+        System.out.println(prefix + (isRight ? "|-- " : "\\-- ") + n.val);
+        printTree(prefix + (isRight ? "|   " : "    "), n.right, true);
+        printTree(prefix + (isRight ? "|   " : "    "), n.left, false);
+      }
+    }
+  }
+}
