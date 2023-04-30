a0b4ccdaf1fa24ed8bedee60701c510b2ec4b0f8
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-30 06:40:44 +0000

    Updated RansomNote383.java

--- RansomNote383.java
+++ RansomNote383.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 
 class RansomNote383 {
-  private char[] ransomArray;
-  private char[] magazineArray;
-  private int counter;
+  char[] ransomArray;
+  char[] magazineArray;
+  int counter;
     
   public boolean canConstruct(String ransomNote, String magazine) {
     if (ransomNote.length() > magazine.length()) {
@@ -17,7 +17,8 @@ class RansomNote383 {
     Arrays.sort(magazineArray);
       
     for (int i = 0; i < ransomArray.length; i++) {
-      if (counter == magazineArray.length - 1 && ransomArray[i] != magazineArray[counter]) {
+      if (counter == magazineArray.length - 1 && 
+        ransomArray[i] != magazineArray[counter]) {
         return false;
       }
 
