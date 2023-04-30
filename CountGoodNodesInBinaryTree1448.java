9ede6e4ca015f7618210173ce3c69926dea98796
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-04-30 00:39:05 +0000

    Added CountGoodNodesInBinaryTree1448.java

---
+++ CountGoodNodesInBinaryTree1448.java
@@ -0,0 +1,43 @@
+class CountGoodNodesInBinaryTree1448 {
+  public int goodNodes(TreeNode root) {
+    return dfs(root, root.val);
+  }
+
+  private int dfs(TreeNode n, int maxVal) {
+    if (n == null) {
+      return 0;
+    }
+
+    int res = n.val >= maxVal ? 1 : 0;
+    maxVal = Math.max(maxVal, n.val);
+    res += dfs(n.left, maxVal);
+    res += dfs(n.right, maxVal);
+
+    return res;
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+
+    public void printTree() {
+      printTree("", this, false);
+    }
+    
+    public void printTree(String prefix, TreeNode n, boolean isRight) {
+      if (n != null) {
+        System.out.println(prefix + (isRight ? "|-- " : "\\-- ") + n.val);
+        printTree(prefix + (isRight ? "|   " : "    "), n.right, true);
+        printTree(prefix + (isRight ? "|   " : "    "), n.left, false);
+      }
+    }
+  }
+}
