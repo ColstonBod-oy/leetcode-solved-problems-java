3d93cb628ac18eee7d9a50ba20fe823b5efed8bd
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-06 12:15:01 +0000

    Updated SubtreeOfAnotherTree572.java

--- SubtreeOfAnotherTree572.java
+++ SubtreeOfAnotherTree572.java
@@ -3,7 +3,8 @@ class SubtreeOfAnotherTree572 {
     if (subRoot == null || isSameTree(root, subRoot)) return true;
     if (root == null) return false;
 
-    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    return isSubtree(root.left, subRoot) || 
+      isSubtree(root.right, subRoot);
   }
 
   private boolean isSameTree(TreeNode p, TreeNode q) {
@@ -11,11 +12,13 @@ class SubtreeOfAnotherTree572 {
       return true;
     }
     
-    if (p == null || q == null || p.val != q.val) {
-      return false;
+    if (p == null || q == null || 
+        p.val != q.val) {
+          return false;
     }
 
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    return isSameTree(p.left, q.left) && 
+      isSameTree(p.right, q.right);
   }
 
   public class TreeNode {
