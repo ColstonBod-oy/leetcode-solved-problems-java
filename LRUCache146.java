91865c6f848b761429d51012039c9a16b679ebe0
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-06 12:43:32 +0000

    Updated LRUCache146.java

--- LRUCache146.java
+++ LRUCache146.java
@@ -66,12 +66,4 @@ class LRUCache146 {
       }
     }
   }
-
-  /**
-   * Your LRUCache object will be instantiated and called as such:
-   * LRUCache obj = new LRUCache(capacity);
-   * int param_1 = obj.get(key);
-   * obj.put(key,value);
-   */
-  
 }
