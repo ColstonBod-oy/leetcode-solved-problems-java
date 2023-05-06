5424781bc00558f0a6ddea99e46a0304d61f4b9d
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-06 12:10:53 +0000

    Updated SameTree100.java

--- SameTree100.java
+++ SameTree100.java
@@ -4,11 +4,13 @@ class SameTree100 {
       return true;
     }
 
-    if (p == null || q == null || p.val != q.val) {
-      return false;
+    if (p == null || q == null || 
+        p.val != q.val) {
+          return false;
     }
 
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    return isSameTree(p.left, q.left) && 
+      isSameTree(p.right, q.right);
   }
 
   public class TreeNode {
