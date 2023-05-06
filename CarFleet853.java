904a29c8e509e04af3fe26ff46edf137f1bc9d37
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-06 12:39:01 +0000

    Updated CarFleet853.java

--- CarFleet853.java
+++ CarFleet853.java
@@ -10,16 +10,18 @@ class CarFleet853 {
   public int carFleet(int target, int[] position, int[] speed) {
     Supplier<TreeMap<Integer, Integer>> mapSupplier = () -> 
       new TreeMap<>(reverseOrder());
-    Map<Integer, Integer> velocities = IntStream.range(0, position.length)
-      .boxed().collect(toMap(i -> position[i], i -> speed[i],
-      (i, j) -> i, mapSupplier));
+    Map<Integer, Integer> velocities = IntStream
+      .range(0, position.length)
+      .boxed().collect(toMap(i -> position[i], 
+        i -> speed[i], (i, j) -> i, mapSupplier));
     Stack<Double> s = new Stack<>();
     
     for (Map.Entry<Integer, Integer> m : velocities.entrySet()) {
       s.push((double) (target - m.getKey()) / m.getValue());
       
-      if (s.size() > 1 && s.peek() <= s.get(s.size() - 2)) {
-        s.pop();
+      if (s.size() > 1 && s.peek() <= 
+          s.get(s.size() - 2)) {
+            s.pop();
       }
     }
 
