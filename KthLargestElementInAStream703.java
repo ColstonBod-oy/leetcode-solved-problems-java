f04b88c98a3f44226c973ad7aef715dd579f358d
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-08 11:49:24 +0000

    Added KthLargestElementInAStream703.java

---
+++ KthLargestElementInAStream703.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+
+class KthLargestElementInAStream703 {
+  PriorityQueue<Integer> pq;
+  int k;
+  
+  public KthLargestElementInAStream703(int k, int[] nums) {
+    pq = new PriorityQueue<>();
+    this.k = k;
+    
+    for (int n : nums) {
+      pq.offer(n);
+    }
+    
+    while (pq.size() > k) {
+      pq.poll();
+    }
+  }
+    
+  public int add(int val) {
+    pq.offer(val);
+
+    if (pq.size() > k) {
+      pq.poll();
+    }
+
+    return pq.peek();
+  }
+}
