00abba3aefe42c8b55f5741a41e09b81c19de462
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-10 08:51:11 +0000

    Updated KClosestPointsToOrigin973.java

--- KClosestPointsToOrigin973.java
+++ KClosestPointsToOrigin973.java
@@ -2,32 +2,23 @@ import java.util.PriorityQueue;
 
 class KClosestPointsToOrigin973 {
   public int[][] kClosest(int[][] points, int k) { 
-    PriorityQueue<Integer[]> pq = new PriorityQueue<>(
-      (a, b) -> a[0].compareTo(b[0])
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> 
+      Double.compare(
+        Math.pow(a[0], 2) + Math.pow(a[1], 2),
+        Math.pow(b[0], 2) + Math.pow(b[1], 2)
+      )
     );
 
     for (int[] point : points) {
-      int distance = 0;
-      
-      for (int n : point) {
-        distance += Math.pow(n, 2);  
-      }
-
-      Integer[] data = {distance, point[0], point[1]};
-      pq.offer(data);
+      pq.offer(point);
     }
 
-    int i = 0;
-    int[][] res = new int[k][2];
+    int[][] res = new int[k][2]; 
 
-    while (k > 0) {
-      Integer[] data = pq.poll();
-      
-      for (int j = 0; j < 2; j++) {
-        res[i][j] = data[j + 1];
-      }
-
-      ++i; --k;
+    for (int i = 0; i < k; i++) {
+      int[] cur = pq.poll();
+      res[i][0] = cur[0];
+      res[i][1] = cur[1];
     }
 
     return res;
