f56ffd4e286731fc610c9dafca5f69754eb619ba
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-15 12:09:23 +0000

    Added FindMedianFromDataStream295.java

---
+++ FindMedianFromDataStream295.java
@@ -0,0 +1,41 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class FindMedianFromDataStream295 {
+  PriorityQueue<Integer> leftHalf;
+  PriorityQueue<Integer> rightHalf;
+  
+  public FindMedianFromDataStream295() {
+    leftHalf = new PriorityQueue<>(Comparator.reverseOrder());
+    rightHalf = new PriorityQueue<>();
+  }
+    
+  public void addNum(int num) {
+    leftHalf.offer(num);
+
+    if (!leftHalf.isEmpty() && !rightHalf.isEmpty() && 
+        leftHalf.peek() > rightHalf.peek()) {
+          rightHalf.offer(leftHalf.poll());
+    }
+
+    if (leftHalf.size() > rightHalf.size() + 1) {
+      rightHalf.offer(leftHalf.poll());
+    }
+
+    if (rightHalf.size() > leftHalf.size() + 1) {
+      leftHalf.offer(rightHalf.poll());
+    }
+  }
+    
+  public double findMedian() {
+    if (leftHalf.size() > rightHalf.size()) {
+      return leftHalf.peek();
+    }
+
+    if (rightHalf.size() > leftHalf.size()) {
+      return rightHalf.peek();
+    }
+
+    return (double) (leftHalf.peek() + rightHalf.peek()) / 2;
+  }
+}
