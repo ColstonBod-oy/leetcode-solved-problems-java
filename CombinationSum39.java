0b31fd94958582d7897f36b637c63de3a198fd05
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-17 09:30:31 +0000

    Added CombinationSum39.java

---
+++ CombinationSum39.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class CombinationSum39 {
+  List<List<Integer>> res = new ArrayList<>();
+  List<Integer> sub = new ArrayList<>();
+  
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    combinationSum(0, 0, candidates, target);
+    return res;
+  }
+
+  public void combinationSum(int i, int total, int[] candidates, int target) {
+    if (total == target) {
+      res.add(new ArrayList<>(sub));
+      return;
+    }
+
+    if (i == candidates.length || total > target) {
+      return;
+    }
+
+    sub.add(candidates[i]);
+    combinationSum(i, total + candidates[i], candidates, target);
+    sub.remove(sub.size() - 1);
+    combinationSum(i + 1, total, candidates, target);
+  }
+}
