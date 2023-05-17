603aec36590fa5464fa9a8d9c8d5b5f14e8f7e2c
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-17 12:24:18 +0000

    Updated DesignAddAndSearchWordsDataStructure211.java

--- DesignAddAndSearchWordsDataStructure211.java
+++ DesignAddAndSearchWordsDataStructure211.java
@@ -1,52 +1,50 @@
 class DesignAddAndSearchWordsDataStructure211 {
-  class WordDictionary {
-    TrieNode root;
+  TrieNode root;
     
-    public WordDictionary() {
-      root = new TrieNode();
-    }
+  public DesignAddAndSearchWordsDataStructure211() {
+    root = new TrieNode();
+  }
     
-    public void addWord(String word) {
-      TrieNode cur = root;
+  public void addWord(String word) {
+    TrieNode cur = root;
       
-      for (char c : word.toCharArray()) {
-        if (cur.children[c - 'a'] == null) {
-          cur.children[c - 'a'] = new TrieNode();
-        } 
+    for (char c : word.toCharArray()) {
+      if (cur.children[c - 'a'] == null) {
+        cur.children[c - 'a'] = new TrieNode();
+      } 
 
-        cur = cur.children[c - 'a'];
+      cur = cur.children[c - 'a'];
+    }
+
+    cur.isEndOfWord = true;
+  }
+    
+  public boolean search(String word) {
+    return search(0, word, root);
+  }
+
+  public boolean search(int index, String word, TrieNode cur) {
+    for (; index < word.length(); index++) {
+      char c = word.charAt(index);
+
+      if (c == '.') {
+        for (TrieNode n : cur.children) {
+          if (n != null && search(index + 1, word, n)) return true; 
+        }
+
+        return false;
       }
 
-      cur.isEndOfWord = true;
-    }
-    
-    public boolean search(String word) {
-      return search(0, word, root);
-    }
-
-    public boolean search(int index, String word, TrieNode cur) {
-      for (; index < word.length(); index++) {
-        char c = word.charAt(index);
-
-        if (c == '.') {
-          for (TrieNode n : cur.children) {
-            if (n != null && search(index + 1, word, n)) return true; 
-          }
-
+      else {
+        if (cur.children[c - 'a'] == null) {
           return false;
         }
 
-        else {
-          if (cur.children[c - 'a'] == null) {
-            return false;
-          }
-
-          cur = cur.children[c - 'a'];
-        }
+        cur = cur.children[c - 'a'];
       }
-
-      return cur.isEndOfWord;
     }
+
+    return cur.isEndOfWord;
   }
 
   class TrieNode {
