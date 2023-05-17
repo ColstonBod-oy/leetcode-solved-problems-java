39f79664873243d382569d11c10ede9033654f6e
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-17 12:41:55 +0000

    Updated TimeBasedKeyValueStore981.java

--- TimeBasedKeyValueStore981.java
+++ TimeBasedKeyValueStore981.java
@@ -3,45 +3,43 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 class TimeBasedKeyValueStore981 {
-  class TimeMap {
-    HashMap<String, List<Pair<String, Integer>>> map;
+  HashMap<String, List<Pair<String, Integer>>> map;
     
-    public TimeMap() {
-      map = new HashMap<>();
-    }
-    
-    public void set(String key, String value, int timestamp) {
-      if (!map.containsKey(key)) {
-        map.put(key, new ArrayList<>());
-      }
-      
-      map.get(key).add(new Pair(value, timestamp));
-    }
-    
-    public String get(String key, int timestamp) {
-      List<Pair<String, Integer>> list = map
-        .getOrDefault(key, new ArrayList<>(0)); 
-      int l = 0;
-      int r = list.size() - 1;
-      String res = "";
-      
-      while (l <= r) {
-        int m = (l + r) / 2;
-
-        if (list.get(m).getValue() <= timestamp) {
-          res = list.get(m).getKey();
-          l = ++m;
-        }
-
-        else {
-          r = --m;
-        }
-      }
-      
-      return res;
-    }
+  public TimeBasedKeyValueStore981() {
+    map = new HashMap<>();
   }
+    
+  public void set(String key, String value, int timestamp) {
+    if (!map.containsKey(key)) {
+      map.put(key, new ArrayList<>());
+    }
+      
+    map.get(key).add(new Pair(value, timestamp));
+  }
+    
+  public String get(String key, int timestamp) {
+    List<Pair<String, Integer>> list = map
+      .getOrDefault(key, new ArrayList<>(0)); 
+    int l = 0;
+    int r = list.size() - 1;
+    String res = "";
+      
+    while (l <= r) {
+      int m = (l + r) / 2;
 
+      if (list.get(m).getValue() <= timestamp) {
+        res = list.get(m).getKey();
+        l = ++m;
+      }
+
+      else {
+        r = --m;
+      }
+    }
+      
+    return res;
+  }
+  
   class Pair<T, V> {
     T key;
     V value;
