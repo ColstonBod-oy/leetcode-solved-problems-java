92e3bb357abed71f42cfba2be96dbc0fdc96e79a
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-26 09:22:11 +0000

    Added NumberOfIslands200.java

---
+++ NumberOfIslands200.java
@@ -0,0 +1,46 @@
+import java.util.List;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.ArrayDeque;
+
+class NumberOfIslands200 {
+  public int numIslands(char[][] grid) {
+    if (grid.length == 0) return 0;
+    
+    int islands = 0;
+    int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    HashSet<List<Integer>> visited = new HashSet<>();
+    
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == '1' && 
+            !visited.contains(List.of(i, j))) {
+              Deque<int[]> q = new ArrayDeque<>();
+              visited.add(List.of(i, j));
+              q.offerFirst(new int[]{i, j});
+
+              while (!q.isEmpty()) {
+                int[] coor = q.pollFirst();  
+                  
+                for (int[] mod : directions) {
+                  int r = coor[0] + mod[0];
+                  int c = coor[1] + mod[1];
+
+                  if (c >= 0 && c < grid[0].length &&
+                      r >= 0 && r < grid.length && 
+                      grid[r][c] == '1' &&
+                      !visited.contains(List.of(r, c))) {
+                        visited.add(List.of(r, c));
+                        q.offerFirst(new int[]{r, c});
+                  }
+                }
+              }
+
+              islands += 1;
+        }
+      }
+    }
+
+    return islands;
+  }
+}
