7e9b5e79fd859a845e63b0b4e7784080fa52e676
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-30 12:49:06 +0000

    Updated PacificAtlanticWaterFlow417.java

--- graphs/PacificAtlanticWaterFlow417.java
+++ graphs/PacificAtlanticWaterFlow417.java
@@ -1,36 +1,34 @@
 package graphs;
 
 import java.util.List;
-import java.util.HashSet;
 import java.util.ArrayList;
 
 public class PacificAtlanticWaterFlow417 {
   public List<List<Integer>> pacificAtlantic(int[][] heights) {
-    HashSet<List<Integer>> pacVisited = new HashSet<>();
-    HashSet<List<Integer>> atlVisited = new HashSet<>();
+    boolean[][] pacOcean = new boolean
+      [heights.length][heights[0].length];
+    boolean[][] atlOcean = new boolean
+      [heights.length][heights[0].length];
     List<List<Integer>> res = new ArrayList<>();
     
     for (int i = 0; i < heights.length; i++) {
       pacificAtlantic(heights, i, 0, 
-        pacVisited, Integer.MIN_VALUE);
+        pacOcean, Integer.MIN_VALUE);
       pacificAtlantic(heights, i, heights[0].length - 1, 
-        atlVisited, Integer.MIN_VALUE);
+        atlOcean, Integer.MIN_VALUE);
     }
 
     for (int i = 0; i < heights[0].length; i++) {
       pacificAtlantic(heights, 0, i, 
-        pacVisited, Integer.MIN_VALUE);
+        pacOcean, Integer.MIN_VALUE);
       pacificAtlantic(heights, heights.length - 1, i, 
-        atlVisited, Integer.MIN_VALUE);
+        atlOcean, Integer.MIN_VALUE);
     }
 
     for (int i = 0; i < heights.length; i++) {
       for (int j = 0; j < heights[0].length; j++) {
-        List<Integer> cur = List.of(i, j);
-        
-        if (pacVisited.contains(cur) && 
-            atlVisited.contains(cur)) {
-              res.add(cur);
+        if (pacOcean[i][j] && atlOcean[i][j]) {
+          res.add(List.of(i, j));
         }
       }
     }
@@ -38,19 +36,19 @@ public class PacificAtlanticWaterFlow417 {
     return res;
   }
 
-  public void pacificAtlantic(int[][] heights, int r, int c, HashSet<List<Integer>> visited, int prevHeight) {
+  public void pacificAtlantic(int[][] heights, int r, int c, boolean[][] ocean, int prevHeight) {
     if (r < 0 || c < 0 || 
         r == heights.length || 
         c == heights[0].length || 
-        visited.contains(List.of(r, c)) ||
-        heights[r][c] < prevHeight) {
+        ocean[r][c] || heights[r][c] < prevHeight) {
           return;
     }
 
-    visited.add(List.of(r, c));
-    pacificAtlantic(heights, r + 1, c, visited, heights[r][c]);
-    pacificAtlantic(heights, r - 1, c, visited, heights[r][c]);
-    pacificAtlantic(heights, r, c + 1, visited, heights[r][c]);
-    pacificAtlantic(heights, r, c - 1, visited, heights[r][c]);
+    ocean[r][c] = true;
+    
+    pacificAtlantic(heights, r + 1, c, ocean, heights[r][c]);
+    pacificAtlantic(heights, r - 1, c, ocean, heights[r][c]);
+    pacificAtlantic(heights, r, c + 1, ocean, heights[r][c]);
+    pacificAtlantic(heights, r, c - 1, ocean, heights[r][c]);
   }
 }
