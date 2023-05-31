5671a9fb4834ed32bcde3e3f334fe0909803cfdf
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-05-31 11:49:25 +0000

    Updated SurroundedRegions130.java

--- graphs/SurroundedRegions130.java
+++ graphs/SurroundedRegions130.java
@@ -27,11 +27,7 @@ public class SurroundedRegions130 {
         if (board[i][j] == 'O') {
           board[i][j] = 'X';
         }
-      }
-    }
 
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[0].length; j++) {
         if (board[i][j] == 'T') {
           board[i][j] = 'O';
         }
