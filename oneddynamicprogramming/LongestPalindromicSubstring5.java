5e4d3fa806a2f525f891dd495ac8602a9d884e4b
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-06-20 05:38:18 +0000

    Added LongestPalindromicSubstring5.java

---
+++ oneddynamicprogramming/LongestPalindromicSubstring5.java
@@ -0,0 +1,33 @@
+package oneddynamicprogramming;
+
+public class LongestPalindromicSubstring5 {
+  public String longestPalindrome(String s) {
+    String res = "";
+
+    for (int i = 0; i < s.length(); i++) {
+      int resLen = res.length();
+      res = longestPalindrome(s, res, resLen, i, i);
+      
+      resLen = res.length();
+      res = longestPalindrome(s, res, resLen, i, i + 1);
+    }
+
+    return res;
+  }
+
+  public String longestPalindrome(String s, 
+      String res, int resLen, int start, int end) {
+    while (start >= 0 && end < s.length() 
+           && s.charAt(start) == s.charAt(end)) {
+      if (end - start + 1 > resLen) {
+        res = s.substring(start, end + 1);
+        resLen = end - start + 1;
+      }
+
+      --start;
+      ++end;
+    }
+
+    return res;
+  }
+}
