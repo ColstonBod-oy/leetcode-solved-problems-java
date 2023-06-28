972116744155c73540ad8c0326a366b913275f7c
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-06-28 05:00:29 +0000

    Updated CoinChangeII518.java

--- twoddynamicprogramming/CoinChangeII518.java
+++ twoddynamicprogramming/CoinChangeII518.java
@@ -7,16 +7,12 @@ public class CoinChangeII518 {
     int[] dp = new int[amount + 1];
     dp[0] = 1;
 
-    for (int i = coins.length - 1; i >= 0; i--) {
-      int[] newDP = Arrays.copyOf(dp, dp.length);
-
+    for (int coin : coins) {
       for (int a = 1; a < amount + 1; a++) {
-        if (a - coins[i] >= 0) {
-          newDP[a] += newDP[a - coins[i]];  
+        if (coin <= a) {
+          dp[a] += dp[a - coin];  
         }
       }
-
-      dp = newDP;
     }
 
     return dp[amount];
