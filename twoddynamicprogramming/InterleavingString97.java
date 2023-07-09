9c001ed87e1fe1a2e2bc6932df7ee04de4b7f500
ColstonBod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2023-07-09 04:01:32 +0000

    Updated InterleavingString97.java

--- twoddynamicprogramming/InterleavingString97.java
+++ twoddynamicprogramming/InterleavingString97.java
@@ -3,23 +3,26 @@ package twoddynamicprogramming;
 public class InterleavingString97 {
   public boolean isInterleave(String s1, 
                               String s2, String s3) {
-    if (s1.length() + s2.length() != s3.length()) {
+    int lenS1 = s1.length();
+    int lenS2 = s2.length();
+    
+    if (lenS1 + lenS2 != s3.length()) {
       return false;
     }
     
-    boolean[][] dp = new boolean[s1.length() + 1]
-                                [s2.length() + 1];
-    dp[s1.length()][s2.length()] = true;
+    boolean[][] dp = new boolean[lenS1 + 1]
+                                [lenS2 + 1];
+    dp[lenS1][lenS2] = true;
 
-    for (int i = s1.length(); i >= 0; i--) {
-      for (int j = s2.length(); j >= 0; j--) {
-        if (i < s1.length() 
+    for (int i = lenS1; i >= 0; i--) {
+      for (int j = lenS2; j >= 0; j--) {
+        if (i < lenS1 
               && s1.charAt(i) == s3.charAt(i + j) 
               && dp[i + 1][j]) {
           dp[i][j] = true;
         }
 
-        if (j < s2.length() 
+        if (j < lenS2 
               && s2.charAt(j) == s3.charAt(i + j) 
               && dp[i][j + 1]) {
           dp[i][j] = true;
