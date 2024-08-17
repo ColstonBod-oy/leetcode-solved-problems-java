814f767b73445a2ac4061ab70c220a3e87f259d8
Colston Bod-oy <75562733+ColstonBod-oy@users.noreply.github.com>
2024-08-17 09:43:54 +0000

    test commit

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@
 class Main {
   public static void main(String[] args) {
     // System.out.println(new ClassName().solutionMethod());
-    System.out.println("Hello world!");
+    System.out.println("Hello");
   }
 }
